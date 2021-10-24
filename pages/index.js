bf918279dd4a5189fd847cd4ae30f81e24e551d7
wahsidin <wahsidinchandra@yahoo.com>
2021-10-24 20:29:51 +0700

    add pages/index.js styles/global.css

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,15 @@ class ERC20Index extends React.Component {
     render() {
         return (
             <Layout>
-            <div>
-                
-            </div>
+                <div id="home-wrapper">
+                    <h1>ERC20 Token ( Cryptocurrency ) </h1>
+                    <h2>By Wahsidin Tjandra</h2>
+                    <p>
+                        <a target="_blank"href="https://www.udemy.com/course/ethereum-and-solidity-the-complete-guide-for-developer/">
+                            Learn Ethereum, Solidity, Food Supply Chain & Cryptocurrency, The Complete Guide for Developer Course
+                        </a>
+                    </p>
+                </div>
             </Layout>
         );
     }
